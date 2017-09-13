a4f640c2b37d9b6496431261dd557ca30c256f6e
Aaron Russell <russell.aaron94@gmail.com>
2017-09-13 17:32:45 -0600

    Added code to bring in CSS; barely started on login view

--- server.js
+++ server.js
@@ -3,12 +3,19 @@
 const express = require("express");
 const app = express();
 
+const minify = require("express-minify");
 const routes = require("./routes");
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use("/", routes);
+
+app.use(minify({
+    onerror: (error) => { console.error(error); }
+}));
+
+app.use(express.static(__dirname + "/assets"));
 
 app.listen(8080, () => {
     console.log("Listening at http://localhost:8080");
